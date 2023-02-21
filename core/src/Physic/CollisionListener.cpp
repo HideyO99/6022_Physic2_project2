a430d8cae3e5197b249609a4a5aee5d9cd9bf8fd
Siraphong <hideyo99@hotmail.com>
2023-02-20 19:37:07 -0500

    update sound but still now fixes

--- core/src/Physic/CollisionListener.cpp
+++ core/src/Physic/CollisionListener.cpp
@@ -10,7 +10,7 @@ CollisionListener::~CollisionListener()
 
 void CollisionListener::notifyCollision(iCollision* body1, iCollision* body2)
 {
-	
+	fmod->stop_sound(FX1_CH);
 	fmod->play_sound("fx1", FX1_CH);
 
 }
